d2f7502258ab23fb219ec73d755ef8f01214efa7
AUDREY PATTERSON <audrey.patterson31@gmail.com>
2021-06-21 09:43:18 -0700

    more build out

--- src/app.js
+++ src/app.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 const App = () => {
   return (
     <>
-    <h1>hey</h1>
+      <BrowserRouter>
+        <h1>hey girl hey!</h1>
+      </BrowserRouter>
     </>
   )
 }
