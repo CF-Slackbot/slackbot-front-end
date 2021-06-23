3898015850a692a0e08c99eeea3f47d54823220a
FIZZO PANNOSCH <fizzo999@gmail.com>
2021-06-23 15:29:23 -0700

    questions changed to new questions input form

--- src/components/questions/questions-edit-form.js
+++ src/components/questions/questions-edit-form.js
@@ -1,11 +1,15 @@
 import React, { useState } from 'react';
 import { Form, Card, Button } from 'react-bootstrap';
 import useForm from '../../hooks/form';
+import { If, Then } from 'react-if';
+import './questions-edit-form.css';
 
-const QuestionsForm = props => {
+const QuestionsEditForm = props => {
   // const [ handleSubmit, handleChange, values ] = useForm(props.addQuestion);
-  const [handleSubmit, handleChange, values] = useForm(props.addQuestion);
+  const [handleSubmit, handleChange, values] = useForm(props.editQuestion);
+  // const [inputFields, setInputFields] = useState({});
   const [inputFields, setInputFields] = useState([{ answerOption: '' }]);
+  const [modalFields, setModalFields] = useState(props.itemId);
 
   const answerLettersArr = ['A', 'B', 'C', 'D', 'E', 'F'];
 
@@ -26,6 +30,7 @@ const QuestionsForm = props => {
     values[index].answerOption = event.target.value;
     setInputFields(values);
     handleChange(event);
+    setModalFields(...values);
   };
 
   const handleTheSubmit = event => {
@@ -38,6 +43,16 @@ const QuestionsForm = props => {
     }
   };
 
+  console.log(
+    'here is the selectedItem FOOOOOOORM ============>>>>>>>>>',
+    props.itemId
+  );
+  console.log(
+    'here is the selectedItem inputFIELDSSSSSSSSSS ============>>>>>>>>>',
+    modalFields,
+    values
+  );
+
   return (
     <Card style={{ width: '18rem' }}>
       <Form onSubmit={handleTheSubmit}>
@@ -46,7 +61,7 @@ const QuestionsForm = props => {
           <Form.Control
             onChange={handleChange}
             type='text'
-            placeholder='Enter a new question'
+            placeholder={props.itemId.question}
             name='question'
             required
             isInvalid
@@ -93,7 +108,7 @@ const QuestionsForm = props => {
             type='text'
             name='answer_a'
             key='answer_a'
-            placeholder='Enter a new question'
+            placeholder={props.itemId.answers.answer_a}
             onChange={handleChange}
             required
             isInvalid
@@ -102,11 +117,56 @@ const QuestionsForm = props => {
             type='text'
             name='answer_b'
             key='answer_b'
-            placeholder='Enter a new question'
+            placeholder={props.itemId.answers.answer_b}
             onChange={handleChange}
             required
             isInvalid
           />
+          <If condition={props.itemId.answers.answer_c}>
+            <Form.Control
+              type='text'
+              name='answer_c'
+              key='answer_c'
+              placeholder={props.itemId.answers.answer_c}
+              onChange={handleChange}
+              required
+              isInvalid
+            />
+          </If>
+          <If condition={props.itemId.answers.answer_d}>
+            <Form.Control
+              type='text'
+              name='answer_d'
+              key='answer_d'
+              placeholder={props.itemId.answers.answer_d}
+              onChange={handleChange}
+              required
+              isInvalid
+            />
+          </If>
+          <If condition={props.itemId.answers.answer_e}>
+            <Form.Control
+              type='text'
+              name='answer_e'
+              key='answer_e'
+              placeholder={props.itemId.answers.answer_e}
+              onChange={handleChange}
+              required
+              isInvalid
+            />
+          </If>
+          <If condition={props.itemId.answers.answer_f}>
+            <Form.Control
+              type='text'
+              name='answer_f'
+              key='answer_f'
+              placeholder={props.itemId.answers.answer_f}
+              onChange={handleChange}
+              required
+              isInvalid
+            />
+          </If>
+
           <button
             className='btn btn-link'
             type='button'
@@ -119,7 +179,7 @@ const QuestionsForm = props => {
               <Form.Control
                 type='text'
                 key={`${inputFields}~${index}`}
-                placeholder='Enter a new question'
+                placeholder={`enter another answer here`}
                 value={inputFields.answerOption}
                 onChange={event => handleInputChange(index, event)}
                 name={`answer_${answerLettersArr[index + 2].toLowerCase()}`}
@@ -144,6 +204,10 @@ const QuestionsForm = props => {
               label={answerLettersArr[0]}
               key='A'
               name='correct_answer'
+              checked={
+                props.itemId.correct_answer ===
+                `answer_${answerLettersArr[0].toLowerCase()}`
+              }
               value={`answer_${answerLettersArr[0].toLowerCase()}`}
               onChange={handleChange}
             />
@@ -152,6 +216,10 @@ const QuestionsForm = props => {
               label={answerLettersArr[1]}
               key='B'
               name='correct_answer'
+              checked={
+                props.itemId.correct_answer ===
+                `answer_${answerLettersArr[1].toLowerCase()}`
+              }
               value={`answer_${answerLettersArr[1].toLowerCase()}`}
               onChange={handleChange}
             />
@@ -161,6 +229,10 @@ const QuestionsForm = props => {
                 label={answerLettersArr[index + 2]}
                 key={`${item}~${index}`}
                 name='correct_answer'
+                checked={
+                  props.itemId.correct_answer ===
+                  `answer_${answerLettersArr[index + 2].toLowerCase()}`
+                }
                 value={`answer_${answerLettersArr[index + 2].toLowerCase()}`}
                 onChange={handleChange}
               />
@@ -183,4 +255,4 @@ const QuestionsForm = props => {
   );
 };
 
-export default QuestionsForm;
+export default QuestionsEditForm;
--- src/components/questions/questions-modal.js
+++ src/components/questions/questions-modal.js
@@ -1,29 +1,89 @@
-import React, { useState, useContext } from 'react';
+import React, { useState, useContext, useCallback } from 'react';
 import { Modal } from 'react-bootstrap';
-import QuestionsForm from './questions-form.js';
-import { SettingsContext } from '../../context/settings'
+import QuestionsEditForm from './questions-edit-form.js';
+import { SettingsContext } from '../../context/settings';
+import useAjax from '../../hooks/ajax.js';
 
-const QuestionsModal = (props) => {
-
+const QuestionsModal = props => {
   const context = useContext(SettingsContext);
 
-  return (
+  // const [list, setList] = useState([]);
+  const { setOptions, response } = useAjax();
+  const qAPI =
+    'https://cf-slackbot-questions-api.herokuapp.com/api/v2/question';
 
+  const updateQuestion = useCallback(
+    async val => {
+      let tagValues = val.tags ? val.tags.replace(' ', '').split(',') : null;
+      let newTagArr = val.tag
+        ? tagValues.map(item => {
+            name: item;
+          })
+        : null;
+
+      if (val.difficulty === '1') {
+        val.difficulty = 'Easy';
+      } else if (val.difficulty === '2') {
+        val.difficulty = 'Medium';
+      } else if (val.difficulty === '3') {
+        val.difficulty = 'Hard';
+      } else {
+        val.difficulty = 'Easy';
+      }
+
+      const options = {
+        method: 'put',
+        url: qAPI,
+        data: {
+          question: val.question,
+          answers: {
+            answer_a: val.answer_a,
+            answer_b: val.answer_b,
+            answer_c: val.answer_c ? val.answer_c : null,
+            answer_d: val.answer_d ? val.answer_d : null,
+            answer_e: val.answer_e ? val.answer_e : null,
+            answer_f: val.answer_f ? val.answer_f : null,
+          },
+          correct_answer: val.correct_answer,
+          tags: newTagArr,
+          category: val.category,
+          difficulty: val.difficulty,
+        },
+      };
+      await setOptions(options);
+      // on successful update of the api it should update the list of questions again - either call out to get or update the list in state with newList = newlist.find(!updatedItem) - newList.push(options.data) setList(newList);
+      context.changeModalDisplay(false);
+    },
+    [setOptions]
+  );
+
+  console.log('context??', context);
+  console.log(
+    'HERE IS THE ITEM ID=========>>>>><<<<<<<<<===========',
+    props.itemId
+  );
+
+  // const editQuestion = () => {};
+
+  return (
     <Modal
       show={context.showModal}
       onHide={() => context.changeModalDisplay(false)}
-      size="md"
-      aria-labelledby="contained-modal-title-vcenter"
+      size='md'
+      aria-labelledby='contained-modal-title-vcenter'
       centered
     >
       <Modal.Header closeButton>
         <Modal.Title>Update Form</Modal.Title>
       </Modal.Header>
       <Modal.Body>
-        <QuestionsForm />
+        <QuestionsEditForm
+          editQuestion={updateQuestion}
+          itemId={props.itemId}
+        />
       </Modal.Body>
     </Modal>
-  )
-}
+  );
+};
 
 export default QuestionsModal;
