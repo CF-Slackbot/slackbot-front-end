31e3ccd3556d77b869355eeb6130d515d18d70cc
AUDREY PATTERSON <audrey.patterson31@gmail.com>
2021-06-22 22:32:10 -0700

    brought in users on the admin and get call to api

--- src/components/adminPage/admin.js
+++ src/components/adminPage/admin.js
@@ -1,44 +1,69 @@
-import React from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import useAjax from '../../hooks/ajax'
 import AdminForm from './admin-form';
 
 const Admin = () => {
 
-  const { setOptions, response, error } = useAjax();
-  const qAPI = "https://dev-d6ditd3b.us.auth0.com/api/v2/users"
+  const [userList, setUserList] = useState([]);
+  const { setOptions, response } = useAjax();
 
-  const addUser = async(user) => {
-    console.log("IS THIS USER?",user)
-    try { 
+  const qAPI = "https://dev-d6ditd3b.us.auth0.com/api/v2/users";
+  const uAPI = process.env.REACT_APP_USER_URL;
+
+  const getUsers = useCallback(async () => {
+    const options = {
+      method: 'get',
+      url: uAPI,
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${process.env.REACT_APP_TOKEN}`
+      }
+    };
+    setOptions(options)
+  }, [setOptions])
+
+  useEffect(() => getUsers(), [getUsers]);
+
+  useEffect(() => {
+    if (response) {
+      response && setUserList(response);
+    }
+  }, [response, getUsers, setUserList]);
+
+  console.log('user list', userList);
+
+  const addUser = async (user) => {
+    console.log("IS THIS USER?", user)
+    try {
       const options = {
-        method:'post',
+        method: 'post',
         url: qAPI,
-        headers: { 
-          'Content-Type':'application/json', 
-          'Authorization':`Bearer ${process.env.REACT_APP_TOKEN}`
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': `Bearer ${process.env.REACT_APP_TOKEN}`
         },
         // dataType: 'json',
         data: {
           email: user.email,
           connection: 'CF-Slackbot-DB',
-          password: user.password, 
-          user_metadata: { 
-            role: user.role 
+          password: user.password,
+          user_metadata: {
+            role: user.role
           }
         }
       }
-    console.log()
-    console.log("OPTIONS",options)
-    await setOptions(options)
-    } catch(e) {
+      console.log()
+      console.log("OPTIONS", options)
+      await setOptions(options)
+    } catch (e) {
       console.error(e.message)
     }
   }
 
   return (
     <>
-    <h1>admin</h1>
-    <AdminForm addUser={addUser}/>
+      <h1>admin</h1>
+      <AdminForm addUser={addUser} />
     </>
   )
 }
