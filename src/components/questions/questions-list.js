53a2488d6c3bdd36b12228db2051ac0632b026c8
Ellis Yoo <yjoo.eyoo@gmail.com>
2021-06-23 18:51:31 -0700

    style for questions page

--- src/components/questions/questions-list.js
+++ src/components/questions/questions-list.js
@@ -1,42 +1,45 @@
-import React, { useState, useCallback, useEffect, useContext } from 'react';
+import React, { useState, useCallback, useEffect, useContext } from "react";
 // import { Form, Card, Button } from 'react-bootstrap';
-import QuestionsModal from './questions-modal.js';
-import { SettingsContext } from '../../context/settings.js';
+import QuestionsModal from "./questions-modal.js";
+import { SettingsContext } from "../../context/settings.js";
+import { ListGroup, Button } from "react-bootstrap";
 
-const QuestionsList = props => {
+const QuestionsList = (props) => {
   const context = useContext(SettingsContext);
-  const [itemId, setItemId] = useState('');
+  const [itemId, setItemId] = useState("");
 
-  const updateItem = e => {
+  const updateItem = (e) => {
     // console.log(
     //   'here is e.target.value <<<<<<<<===============',
     //   e.target.value
     // );
     // console.log('here is itemId <<<<<<<<===============', itemId);
     let selectedItem = props.questionsList.filter(
-      item => item._id == e.target.value
+      (item) => item._id == e.target.value
     );
-    console.log('here is selectedItem <<<<<<<<===============', selectedItem);
+    console.log("here is selectedItem <<<<<<<<===============", selectedItem);
     setItemId(selectedItem[0]);
     context.changeModalDisplay(true);
   };
 
-  console.log('here is questionsList', props.questionsList);
+  console.log("here is questionsList", props.questionsList);
 
   return (
     <>
-      <ul>
+      <ListGroup>
         {props.questionsList.map((item, index) => (
-          <li key={index}>
+          <ListGroup.Item key={index} action>
             {item.question}
-            <button onClick={() => props.deleteQuestion(item._id)}>X</button>
-            <button onClick={updateItem} value={item._id}>
-              edit
-            </button>
-          </li>
+            <span className="delete-edit-btn">
+              <Button variant="outline-danger" onClick={() => props.deleteQuestion(item._id)}>X</Button>{' '}
+              <Button variant="outline-success" onClick={updateItem} value={item._id}>
+                edit
+              </Button>
+            </span>
+          </ListGroup.Item>
         ))}
         <QuestionsModal itemId={itemId} />
-      </ul>
+      </ListGroup>
     </>
   );
 };
